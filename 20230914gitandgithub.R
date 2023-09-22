b6acc6d7aef0910103ac407df8ae6a85e95cb3e8
FelixLiucumc <yl5508@cumc.columbia.edu>
2023-09-22 16:38:52 -0400

    adding code note (Sep 14)

---
+++ 20230914gitandgithub.R
@@ -0,0 +1,13 @@
+####################
+# Sep 14 2023, Tue
+# In class coding
+#
+# chapter: git and github
+####################
+
+usethis::use_git_config(user.name = "FelixLiucumc", user.email = "yl5508@cumc.columbia.edu")
+
+usethis::create_github_token()
+#alternatively, we can run
+gitcreds::gitcreds_set()
+#token: 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 (expires on Oct 14)
